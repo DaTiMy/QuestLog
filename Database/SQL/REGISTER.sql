1287151ca28856bed9de531895df160ef9cef497
DaTiMy <37105868+DaTiMy@users.noreply.github.com>
2022-05-02 16:56:58 +0200

    JSON Format

---
+++ Database/SQL/REGISTER.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO User(name,username,password,slots,sessiontoken) VALUES(
+"Tim",
+"daTimy",
+"$2a$12$lYWw4rIBOmQIfzkvNmJdg.vfs5P3ImNkQSBZaFZlwKWl01SZlSExC",
+3,
+"dsuiouiodhsaooi");
+
+INSERT INTO User(name,username,password,slots,sessiontoken) VALUES(
+"Toni",
+"Sise",
+"$2a$12$lYWw4rIBOmQIfzkvNmJdg.vfs5P3ImNkQSBZaFZlwKWl01SZlSExC",
+3,
+"dsuiouiodhsaooi32133");
+
+SELECT username FROM User WHERE username = 
