e274fb09a928595c501981715c2d0bb07b7fba45
SirBloody <70984011+SirBloody@users.noreply.github.com>
2022-05-03 12:29:54 +0200

    UI works & first JSON tests

---
+++ QuestLog/QuestLog/Connection.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestLog
+{
+    internal class Connection
+    {
+        public static List<Quest> GetQuestList(int SID)
+        {
+            string json = "";
+            List<Quest> q = new List<Quest>();
+
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString("http://172.16.47.13:5000/quests/select/sid/" + SID);
+            }
+
+            
+
+            return q;
+        }
+
+        public static List<Quest> GetQuestListTEST(int SID)
+        {
+            List<Quest> quests = Enumerable.Range(1, 3)
+            .Select(x => new Quest()
+            {
+                Name = "Quest " + x,
+                OrderNumber = x,
+                EXP = 100,
+                Subquests = Enumerable.Range(1, 2)
+                            .Select(y => new SubQuest()
+                            {
+                                Name = "SubQuest " + y,
+                                Nr = y
+                            }).ToList()
+            }).ToList();
+
+            string json = JsonConvert.SerializeObject(quests, Formatting.Indented);
+
+            return quests;
+
+        }
+    }
+}
--- QuestLog/QuestLog/Quest.cs
+++ QuestLog/QuestLog/Quest.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuestLog
-{
+{   
+    [Serializable]
+
     internal class Quest
     {
+        [JsonProperty("copper")]
+        public int Copper { get; set; }
+
+        [JsonProperty("exp")]
+        public int EXP { get; set; }
+
+        [JsonProperty("finish")]
+        public bool Finish { get; set; }
+
+        [JsonProperty("gold")]
+        public int Gold { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
 
-        public int Nr { get; set; }
+        [JsonProperty("ordernumber")]
+        public int OrderNumber { get; set; }
 
+        [JsonProperty("qid")]
+        public int QID { get; set; }
+
+        [JsonProperty("silver")]
+        public int Silver { get; set; }
+
+        [JsonProperty("subquests")]
         public List<SubQuest> Subquests { get; set; }
 
     }
