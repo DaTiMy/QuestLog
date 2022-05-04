e38c5582890ad8d7c8b63cc06152480273658b85
DaTiMy <37105868+DaTiMy@users.noreply.github.com>
2022-05-04 11:41:16 +0200

    User Stable 0.2

--- QuestLogAPI/QuestLogAPI/blueprints/user_blueprint.py
+++ QuestLogAPI/QuestLogAPI/blueprints/user_blueprint.py
@@ -1,9 +1,25 @@
 from typing import Tuple
-from flask import Blueprint, Response, Request, jsonify
+from flask import Blueprint, Response, Request, jsonify, request
 from connect.connect import connection
 import collections
+import jsonschema
+import json
+from jsonschema import validate
+import bcrypt
 
 user = Blueprint('user', __name__, url_prefix='/users')
+
+
+userSchema = {
+
+    "properties": {
+        "Email": {"type": "integer"},
+        "Name": {"type": "string"},
+        "Password": {"type": "string"}
+        
+    },
+  "required": ["Email", "Name", "Password"]
+}
 
 
 
@@ -40,7 +56,7 @@ def get_userdata(username):
 
    
    
- 
+    cur.close()
     con.close()
 
     return jsonify(insertObject)
@@ -51,4 +67,42 @@ def get_userdata(username):
 @user.route('/register', methods=['POST'])
 def insert_register():
 
+    content = request.json
+
+    isValidate = validateRegisterJson(content)
+
+    if not isValidate:
+        return quest.register_error_handler(400, handle_bad_request)
+
+    contentDict = json.loads(request.data)
+
+    hashed = hashPassword(contentDict['Password'])
+
+    con = connection()
+    cur = con.cursor()
+    sql = """INSERT INTO User (Name, Username, Password, Slots) VALUE (%s, %s, %s, %s)"""
+    cur.execute(sql,(contentDict['Name'], contentDict['Username'], hashed, 3))
+    con.commit()
+    cur.close()
+    con.close()
+
+
     return "Success"
+
+
+
+#functions
+def hashPassword(passw):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(passw, salt)
+
+    return hashed
+
+
+
+def validateRegisterJson(jsonData):
+    try:
+        validate(instance=jsonData, schema=questSchema)
+    except jsonschema.exceptions.ValidationError as err:
+        return False
+    return True
