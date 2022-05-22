fa7599568ddbfc00d9c9b4a185018168b1d2fb46
DaTiMy <37105868+DaTiMy@users.noreply.github.com>
2022-05-23 00:23:36 +0200

    request.json AWS (API)

--- QuestLogAPI/QuestLogAPI/blueprints/quest_blueprint.py
+++ QuestLogAPI/QuestLogAPI/blueprints/quest_blueprint.py
@@ -44,7 +44,7 @@ def addQuest(SID):
         res.status_code = 400
         return res
    
-    contentDict = json.loads(request.data)
+    contentDict = json.loads(request.json)
 
 
     sql = """INSERT INTO Quest (sid,name,exp,copper,silver,gold,finish,ordernumber) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
@@ -85,7 +85,7 @@ def addSubQuest(QID):
         res.status_code = 400
         return res
    
-    contentDict = json.loads(request.data)
+    contentDict = json.loads(request.json)
 
     sql = """INSERT INTO SubGoal (qid,name,description,finish,ordernumber) VALUES (%s, %s, %s, %s, %s)"""
 
